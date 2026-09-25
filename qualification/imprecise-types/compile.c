d4a1935dd6a21f48e76fd4bc31ee53577f03ab40
i <isomorphisms@sdf.org>
2026-09-24 17:00:16 -0700

    Keep imprecise compile probe freestanding

--- qualification/imprecise-types/compile.c
+++ qualification/imprecise-types/compile.c
@@ -1,17 +1,16 @@
-#include <stdint.h>
 #include <ick/imprecise.h>
 
 _Static_assert(sizeof(E3M2) == 1, "E3M2 must occupy one byte in C objects");
 _Static_assert(sizeof(E5M3) == 1, "E5M3 must occupy one byte in C objects");
 
-uint8_t
-ick_e3m2_round_trip_code(uint8_t code)
+ick_byte
+ick_e3m2_round_trip_code(ick_byte code)
 {
     return e3m2_code(e3m2_from_float(e3m2_to_float(e3m2_from_code(code))));
 }
 
 int
-ick_e5m3_round_trip_code(uint8_t code, uint8_t *round_tripped)
+ick_e5m3_round_trip_code(ick_byte code, ick_byte *round_tripped)
 {
     E5M3 encoded = e5m3_from_code(code);
     E5M3 result;
