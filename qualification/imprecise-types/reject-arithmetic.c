681fa95d6d6b32ab1495079c429e7d938e8e3c85
i <isomorphisms@sdf.org>
2026-09-24 17:14:06 -0700

    Reject storage and circle category mistakes

--- qualification/imprecise-types/reject-arithmetic.c
+++ qualification/imprecise-types/reject-arithmetic.c
@@ -1,17 +1,24 @@
 #include <ick/imprecise.h>
+#include <ick/circle.h>
 
-#if defined(TEST_E3)
-E3M2
-bad_e3m2_add(E3M2 left, E3M2 right)
+#if defined(TEST_E5M3)
+E5M3
+bad_e5m3_add(E5M3 left, E5M3 right)
 {
     return left + right;
 }
-#elif defined(TEST_E5)
-E5M3
-bad_e5m3_multiply(E5M3 left, E5M3 right)
+#elif defined(TEST_CIRCLE)
+Circle96
+bad_circle_add(Circle96 left, Circle96 right)
 {
-    return left * right;
+    return left + right;
+}
+#elif defined(TEST_CIRCLE_ROTATION_MIX)
+Rotation96
+bad_circle_rotation_mix(Circle96 point)
+{
+    return point;
 }
 #else
-#error define TEST_E3 or TEST_E5
+#error define TEST_E5M3, TEST_CIRCLE, or TEST_CIRCLE_ROTATION_MIX
 #endif
